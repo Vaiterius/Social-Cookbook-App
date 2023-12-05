c20720e1abdc11bfd574f5277b1345a6d52805c0
Vaiterius <gerard.gandionco@gmail.com>
2023-12-05 15:23:11 -0800

    Write tests for user following and blocking

--- backend/apps/users/models.py
+++ backend/apps/users/models.py
@@ -1,9 +1,16 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 
 from .managers import UserManager
+
+if TYPE_CHECKING:
+    from apps.users.models import User
 
 
 class User(AbstractUser):
@@ -35,6 +42,43 @@ class User(AbstractUser):
     REQUIRED_FIELDS = ['username']
 
     objects = UserManager()
+
+    def follow(self, user: User) -> bool:
+        """Follow a user if not already following and not blocked by them"""
+        if self.is_following(user) or user.has_blocked(self):
+            return False
+        self.following.add(user)
+        return True
+
+    def unfollow(self, user: User) -> bool:
+        """Unfollow a user if already following and not blocked by them"""
+        if not self.is_following(user) or user.has_blocked(self):
+            return False
+        self.following.remove(user)
+        return True
+
+    def block(self, user: User) -> bool:
+        """Block a user if not already blocked"""
+        if self.has_blocked(user):
+            return False
+        self.blocking.add(user)
+        return True
+
+    def unblock(self, user: User) -> bool:
+        """Unblock a user if already blocked"""
+        if not self.has_blocked(user):
+            return False
+        self.blocking.remove(user)
+        return True
+    
+    def is_following(self, user: User) -> bool:
+        followed_user: User = self.following.filter(username=user.username)
+        return followed_user.first() is not None
+
+    def has_blocked(self, user: User) -> bool:
+        """See whether this user has blocked that user"""
+        blocked_user: User = self.blocking.filter(username=user.username)
+        return blocked_user.first() is not None
 
     def __str__(self) -> str:
         return self.username
--- backend/apps/users/tests.py
+++ backend/apps/users/tests.py
@@ -1,3 +1,6 @@
+import random
+import string
+
 from django.contrib.auth import get_user_model
 from django.test import TestCase
 
@@ -6,21 +9,122 @@ from apps.recipes.models import Recipe
 
 class UserTests(TestCase):
 
+    def _get_random_user(self):
+        random_username: str = "".join(
+            random.choice(string.ascii_letters + string.digits)
+            for _ in range(8)
+        )
+        return get_user_model().objects.create_user(
+            username=random_username,
+            email=f"{random_username}@gmail.com",
+            password="1234"
+        )
+
     def setUp(self):
-        pass
+        self.user = get_user_model().objects.create_user(
+            username="taylor_swift",
+            email="tswift@gmail.com",
+            password="1234"
+        )
 
     def test_following_system(self):
-        pass
+        follower_1 = self._get_random_user()
+        follower_2 = self._get_random_user()
+        follower_3 = self._get_random_user()
+
+        # Test following.
+        follower_1.follow(self.user)
+        follower_2.follow(self.user)
+        follower_3.follow(self.user)
+
+        self.assertEqual(len(self.user.followers.all()), 3)
+        self.assertTrue(follower_1.is_following(self.user))
+
+        # Do it again, nothing should change.
+        follower_1.follow(self.user)
+        follower_2.follow(self.user)
+        follower_3.follow(self.user)
+
+        self.assertEqual(len(self.user.followers.all()), 3)
+        self.assertTrue(follower_2.is_following(self.user))
+
+        # Test unfollowing.
+        follower_1.unfollow(self.user)
+        follower_2.unfollow(self.user)
+        follower_3.unfollow(self.user)
+
+        self.assertEqual(len(self.user.followers.all()), 0)
+        self.assertEqual(len(follower_2.following.all()), 0)
+        self.assertFalse(follower_3.is_following(self.user))
+
+        # Do it again, nothing should change.
+        follower_1.unfollow(self.user)
+        follower_2.unfollow(self.user)
+        follower_3.unfollow(self.user)
+
+        self.assertEqual(len(self.user.followers.all()), 0)
+        self.assertEqual(len(follower_2.following.all()), 0)
+        self.assertFalse(follower_3.is_following(self.user))
+
 
     def test_blocking_system(self):
-        pass
+        hater_1 = self._get_random_user()
+        hater_2 = self._get_random_user()
+        hater_3 = self._get_random_user()
+
+        self.assertTrue(not hater_1.has_blocked(self.user))
+        self.assertTrue(not self.user.has_blocked(hater_1))
+        self.assertEqual(self.user.blocking.count(), 0)
+        self.assertEqual(hater_2.blocking.count(), 0)
+
+        # Test blocking.
+        self.user.block(hater_1)
+        self.user.block(hater_2)
+
+        self.assertEqual(self.user.blocking.count(), 2)
+        self.assertTrue(self.user.has_blocked(hater_1))
+        self.assertTrue(self.user.has_blocked(hater_2))
+        self.assertFalse(self.user.has_blocked(hater_3))
+
+        # Attempts to follow shouldn't work.
+        hater_1.follow(self.user)
+        hater_2.follow(self.user)
+
+        self.assertFalse(hater_1.is_following(self.user))
+        self.assertFalse(hater_2.is_following(self.user))
+
+        # Do it again, nothing should change.
+        self.user.block(hater_1)
+        self.user.block(hater_2)
+
+        self.assertEqual(self.user.blocking.count(), 2)
+        self.assertTrue(self.user.has_blocked(hater_1))
+        self.assertTrue(self.user.has_blocked(hater_2))
+        self.assertFalse(self.user.has_blocked(hater_3))
+
+        # Test unblocking.
+        self.user.unblock(hater_1)
+        self.user.unblock(hater_2)
+
+        self.assertEqual(self.user.blocking.count(), 0)
+        self.assertFalse(self.user.has_blocked(hater_1))
+        self.assertFalse(self.user.has_blocked(hater_2))
+        self.assertFalse(self.user.has_blocked(hater_3))
+
+        # Do it again, nothing should change.
+        self.user.unblock(hater_1)
+        self.user.unblock(hater_2)
+
+        self.assertEqual(self.user.blocking.count(), 0)
+        self.assertFalse(self.user.has_blocked(hater_1))
+        self.assertFalse(self.user.has_blocked(hater_2))
+        self.assertFalse(self.user.has_blocked(hater_3))
 
 
 class UserManagerTests(TestCase):
 
     def test_create_user(self):
-        User = get_user_model()
-        user = User.objects.create_user(
+        user = get_user_model().objects.create_user(
             username="user123",
             email="user123@gmail.com",
             password="test123"
