c3a916846ec088498b0456b7f5e616816c2ac82c
Vaiterius <gerard.gandionco@gmail.com>
2026-08-22 03:52:17 -0700

    chore: reset project for full rewrite

--- backend/apps/recipes/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Recipe, Instruction, Ingredient, Tag, Cookbook
-
-admin.site.register(Recipe)
-admin.site.register(Instruction)
-admin.site.register(Ingredient)
-admin.site.register(Tag)
-admin.site.register(Cookbook)
